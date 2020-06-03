c808f3113f0edec7b335d115aac9421f0a98e866
Andrew Schok <aaschok@alaska.edu>
2020-06-03 13:49:24 -0800

    new

--- JadeGraph.py
+++ JadeGraph.py
@@ -1,6 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-import os,struct,scipy
+import os,struct,scipy,logging
+
+logging.basicConfig(filename='test.log',filemode='w',level=logging.DEBUG)
 
 class JadeGraph():
     def __init__(self,dataFolder,startTime,endTime):
@@ -11,19 +13,18 @@ class JadeGraph():
     def Graph(self):
         with open(self.dataFolder, "rb") as f:
             species = 3
-            windows = 1440
+            rows = 8640
             timeList = np.array([])
             energyMatrix = []
             countSecMatrix = []
-            for i in range(windows):
-                data = f.read(259872)
+            data = f.read(259872)
+            for i in range(rows):
                 
-
                 byteStart = 66-1
                 byteEnd = byteStart+1
                 dataSlice = data[byteStart:byteEnd]
                 ionSpecies = struct.unpack('b',dataSlice)[0]
-                
+                                
                 if ionSpecies == species:
 
                     byteStart = 1-1
@@ -54,20 +55,20 @@ class JadeGraph():
                         dataSlice = data[byteStart:byteEnd]
                         energy = struct.unpack('f'*78,dataSlice)
                         energyMatrix.append(energy)
+                data = f.read(259872)
 
         countSecMatrix = np.array(countSecMatrix)
         countSecArray = countSecMatrix.flatten()
 
         energyMatrix = np.array(energyMatrix)
         energyArray = energyMatrix.flatten()
-        
+
         plt.plot(energyArray,countSecArray)
         plt.title('Counts Vs. E')
         plt.ylabel('Counts/Sec')
         plt.xlabel('eV/q')
         plt.show()
-        print(len(energyArray))
-        plt.specgram(energyArray,Fs=4992,NFFT=78,noverlap=0)
+        plt.specgram(energyArray,Fs=599040,NFFT=78,noverlap=0)
         plt.title('Energy Spec')
         plt.colorbar()
         plt.show()
@@ -81,7 +82,7 @@ class JadeGraph():
 
 if __name__ == '__main__':
 
-    dataFolder = os.path.join('data','jad','ION_SPECIES','JAD_L30_LRS_ION_ANY_CNT_2017068_V02.DAT')
+    dataFolder = os.path.join('..','data','jad','ION_SPECIES','JAD_L30_LRS_ION_ANY_CNT_2017068_V02.DAT')
     #RJW, Name, Format, dimnum, size dim 1, size dim 2,...
     timeStart = 1
     timeEnd = 2
