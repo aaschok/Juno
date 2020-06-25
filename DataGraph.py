1a1686397a019ec11baf9b130e98a34e8088561a
Andrew Schok <aaschok@alaska.edu>
2020-06-24 17:58:27 -0800

    desktop

--- DataGraph.py
+++ DataGraph.py
@@ -6,8 +6,8 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 def Graph():
 
-    timeStart = '2017-03-08T00:00:00.000'
-    timeEnd = '2017-03-09T23:59:59.000'
+    timeStart = '2017-02-20T00:00:00'
+    timeEnd = '2017-02-20T23:59:59'
 
     orbitsData = {1:'2016-07-31T19:46:02',
                 2:'2016-09-23T03:44:48',
@@ -26,17 +26,17 @@ def Graph():
     
 
     fgm = FGMData(csvFiles,timeStart,timeEnd)
+
     for date in ISO:
         
         fgmStart = 0
         jadStart = 0
         
         for i in range(1,5):
-            fig, (ax1,ax2) = plt.subplots(2,1,sharex=True,figsize=(9,4))
+            fig, (ax1,ax2) = plt.subplots(2,1,sharex=True,figsize=(10,4))
 
             if date in jade.dataDict.keys():
                 jadeData = jade.dataDict[date]  
-
                 logging.debug(jadeData['LAT_ARRAY'])
                 logging.debug(jadeData['DIST_ARRAY'])
 
@@ -89,14 +89,14 @@ def Graph():
 
             if date in fgm.dataDict.keys():
                 fgmData = fgm.dataDict[date]
-
+                
                 fgmIndex = min(range(len(fgmData['TIME_ARRAY'])), key=lambda j: abs(fgmData['TIME_ARRAY'][j]-i*6))
 
                 ax2.plot(fgmData['TIME_ARRAY'][fgmStart:fgmIndex+1],fgmData['BX'][fgmStart:fgmIndex+1],label='$B_x$',linewidth=1)
                 ax2.plot(fgmData['TIME_ARRAY'][fgmStart:fgmIndex+1],fgmData['BY'][fgmStart:fgmIndex+1],label='$B_y$',linewidth=1)
                 ax2.plot(fgmData['TIME_ARRAY'][fgmStart:fgmIndex+1],fgmData['BZ'][fgmStart:fgmIndex+1],label='$B_z$',linewidth=1)
                 ax2.plot(fgmData['TIME_ARRAY'][fgmStart:fgmIndex+1],fgmData['B'][fgmStart:fgmIndex+1],'black',label='$^+_-|B|$',linewidth=0.5)
-                ax2.plot(fgmData['TIME_ARRAY'][fgmStart:fgmIndex+1],-fgmData['B'][fgmStart:fgmIndex+1],'black',linewidth=0.5)
+                ax2.plot(fgmData['TIME_ARRAY'][fgmStart:fgmIndex+1],-np.array(fgmData['B'][fgmStart:fgmIndex+1]),'black',linewidth=0.5)
                 ax2.legend(loc=(1.01,0.1))
                 ax2.set_xlabel('Hrs')
                 ax2.xaxis.set_label_coords(1.04,-0.053)
@@ -116,9 +116,9 @@ def Graph():
                     break
                 
             YDOY = datetime.date.fromisoformat(date).strftime('%Y%j')
-            timeFormatDict = {1:'00000-0600',2:'0600-1200',3:'1200-1800',4:'1800-2400'}
+            timeFormatDict = {1:'0000',2:'0600',3:'1200',4:'1800'}
             
-            fileSaveName = f'jad_fgm_{YDOY}_{timeFormatDict[i]}_orbit{orbitNum}'
+            fileSaveName = f'jad_fgm_{YDOY}_{timeFormatDict[i]}'
 
             plt.savefig(pathlib.Path(f'..\\figures\\orbit{orbitNum}\\{fileSaveName}'),bbox_inches='tight',pad_inches=0.02,dpi=150)
             plt.close(fig)
