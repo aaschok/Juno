c92c5e40fa3a87e77e292ebd1562e5927e4d2426
andrew schok <aaschok@alaska.edu>
2021-03-19 17:22:11 -0800

    Extract data changes

--- crossingextract.py
+++ crossingextract.py
@@ -3,19 +3,18 @@ import pandas as pd
 import numpy as np
 from spacepy import pycdf
 import spiceypy as spice
+from dataclasses import *
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
-class CrossingsDataCDF():
+class CrossingsData():
 
-    def __init__(self,data_folder,fgm_folder):
+    def __init__(self,data_folder):
         self.data_folder = pathlib.Path(data_folder)
-        self.fgm_folder = pathlib.Path(fgm_folder)
         self.crossingdf = None
         self.cdfdf = pd.DataFrame()
-        self.getData()
-        self.getFgmData()
+        self.getCrossings()
         
-
-    def getData(self):
+    def getCrossings(self):
         for file_name in os.listdir(self.data_folder):
             file_path = os.path.join(self.data_folder,file_name)
             if file_path.endswith('.dat'):                
@@ -27,13 +26,17 @@ class CrossingsDataCDF():
                 self.crossingdf = dat
             else:
                 self.crossingdf = self.crossingdf.append(dat,ignore_index=True)
-        #From here down the crossings dates are looked at to find which cooresponding mag data is available for each date
+       
+        
+            
+    def getFGMData(self,fgm_folder):
+        #Mag data for each cooresponding crossing is found
         self.file_dict = {}
         p = re.compile(r'\d{7}')
-        for parent,child,files in os.walk(self.fgm_folder):
+        for parent,child,files in os.walk(fgm_folder):
             for file_name in files:
                 if file_name.endswith('.csv'):
-                    file_path = os.path.join(self.fgm_folder,file_name)
+                    file_path = os.path.join(fgm_folder,file_name)
                     date = p.search(file_name).group()
                     date_iso = datetime.datetime.strptime(date,'%Y%j').strftime('%Y-%m-%d')
                     
@@ -41,12 +44,13 @@ class CrossingsDataCDF():
                     self.cdfdf = self.cdfdf.append(match,ignore_index = True)
                     if date_iso not in self.file_dict.keys():
                         self.file_dict[date_iso] = file_path
-            
-    def getFgmData(self):
+
+        #The mag data is sorted through and two hours on either side of the crossing is pulled
         for row in self.cdfdf.itertuples():
             date = row[1]
             time = row[2]
             type = row[3]
+            
             crossing_stamp = datetime.datetime.strptime(f'{date}T{time}','%Y-%m-%dT%H:%M:%S')
             if date in self.file_dict.keys():
                 
@@ -61,12 +65,18 @@ class CrossingsDataCDF():
                 spice_time_list = [spice.utc2et(i) for i in save_time_list]
                 position_list = []
                 latitude_list = []
+                x,y,z=[],[],[]
                 for spice_time in spice_time_list:
                     pos,lt = spice.spkpos('JUNO',spice_time,'IAU_JUPITER','NONE','JUPITER')
                     pos_vec = spice.vpack(pos[0],pos[1],pos[2])
                     rad_pos,long,lat = spice.reclat(pos_vec)
                     lat *= spice.dpr()
                     rad_pos /= 69911
+
+                    pos,lt = spice.spkpos('JUNO',spice_time,'IAU_SUN','NONE','JUPITER')
+                    x.append(pos[0])
+                    y.append(pos[1])
+                    z.append(pos[2])
 
                     position_list.append(rad_pos)
                     latitude_list.append(lat)
@@ -82,12 +92,12 @@ class CrossingsDataCDF():
                 cdf_file['BY DATA'].attrs['units'] = 'nT'
                 cdf_file['BZ DATA'] = fgm_data['BZ PLANETOCENTRIC'][crossing_index_minus:crossing_index_plus+1]
                 cdf_file['BZ DATA'].attrs['units'] = 'nT'
-                cdf_file['X POSITION'] = [i/69911 for i in fgm_data['X'][crossing_index_minus:crossing_index_plus+1]]
-                cdf_file['X POSITION'].attrs['units'] = 'Rj'
-                cdf_file['Y POSITION'] = [i/69911 for i in fgm_data['Y'][crossing_index_minus:crossing_index_plus+1]]
-                cdf_file['Y POSITION'].attrs['units'] = 'Rj'
-                cdf_file['Z POSITION'] = [i/69911 for i in fgm_data['Z'][crossing_index_minus:crossing_index_plus+1]]
-                cdf_file['Z POSITION'].attrs['units'] = 'Rj'
+                cdf_file['X POSITION'] = x
+                cdf_file['X POSITION'].attrs['units'] = 'km'
+                cdf_file['Y POSITION'] = y
+                cdf_file['Y POSITION'].attrs['units'] = 'km'
+                cdf_file['Z POSITION'] = z
+                cdf_file['Z POSITION'].attrs['units'] = 'km'
                 cdf_file['RADIAL DISTANCE'] = position_list
                 cdf_file['RADIAL DISTANCE'].attrs['units'] = 'Rj'
                 cdf_file['LATITUDE'] = latitude_list
@@ -95,12 +105,35 @@ class CrossingsDataCDF():
 
                 cdf_file.close()
                 print(f'Created CDF for {type} crossing {crossing_stamp.strftime("%Y-%m-%dT%H:%M:%S")}')
-
+    
+    def getJade(self,jade_folder):
+        timeStart = '2017-03-09T00:00:01.500'
+        timeEnd = '2017-03-10T00:00:02.531'
+        dataFolder = pathlib.Path('../data/jad')
+        DOY,ISO,datFiles = getFiles(timeStart,timeEnd,'.DAT',dataFolder,'JAD_L30_LRS_ION_ANY_CNT') 
+        jadeIon = JadeData(datFiles,timeStart,timeEnd)
+        jadeIon.getIonData()
+        cdf_file = pycdf.CDF(r'..\crossings\test.cdf','')
+        for date in jadeIon.dataDict.keys():
+            
+            jade_data = jadeIon.dataDict[date]
+            cdf_file['JADE DATA'] = jade_data['DATA_ARRAY']
+            print(jade_data['DATA_ARRAY'])
+            
+            
 def dataToCDF():
     crossings_folder = r'..\crossings'
     fgm_folder = r'..\data\fgm'
-    Crossings = CrossingsDataCDF(crossings_folder,fgm_folder)
+    crossings = CrossingsData(crossings_folder)
+    crossings.getFGMData(fgm_folder)
 
+
+def jadeTest():
+    crossings_folder = r"E:\Python\Juno Folder\crossings"
+    jad_folder = r"E:\Python\Juno Folder\data\jad"
+    crossings = CrossingsData(crossings_folder)
+    crossings.getJade(jad_folder)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 def dataToPickle():
     orbits_begin = {1:'2016-07-31T19:46:02',
                             2:'2016-09-23T03:44:48',
@@ -160,9 +193,10 @@ def dataToPickle():
         save_path = pathlib.Path(f'..\data\pickledfgm\jno_fgm_{save_name}.pkl')
         pickledf = fgmdf.to_pickle(save_path)
         print(f'Saved pickle {date}')                                     
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    dataToCDF()
+    jadeTest()
     
     
     
